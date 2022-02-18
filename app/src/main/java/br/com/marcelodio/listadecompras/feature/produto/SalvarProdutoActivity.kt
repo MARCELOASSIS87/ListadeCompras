13c6ce036adba2b123da469ffbc26109ef667ab7
Marcelo Assis <marceloassis50@gmail.com>
2022-02-18 19:06:25 -0300

    acerto de gradle

--- app/src/main/java/br/com/marcelodio/listadecompras/feature/produto/SalvarProdutoActivity.kt
+++ app/src/main/java/br/com/marcelodio/listadecompras/feature/produto/SalvarProdutoActivity.kt
@@ -1,7 +1,6 @@
 package br.com.marcelodio.listadecompras.feature.produto
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -9,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import br.com.marcelodio.listadecompras.R
 
 
-class SalvarProdutoActivity : ListaDeProdutosActivity() {
+class SalvarProdutoActivity : AppCompatActivity() {
 
     private lateinit var editNome: EditText
     private lateinit var editQuantidade: EditText
@@ -20,19 +19,11 @@ class SalvarProdutoActivity : ListaDeProdutosActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.adicionar_produto)
-        helper = HelperDB(this)
         initView()
         buttonSalvar.setOnClickListener { salvarProduto() }
 
     }
-
-    private fun initView() {
-        editNome = findViewById(R.id.editTextNomeProduto)
-        editQuantidade = findViewById(R.id.editTextQuantidade)
-        buttonSalvar = findViewById(R.id.buttonSalvar)
-    }
-
-    fun salvarProduto() {
+    private fun salvarProduto() {
         val nome = editNome.text.toString()
         val quantidade = editQuantidade.text.toString()
 
@@ -44,7 +35,6 @@ class SalvarProdutoActivity : ListaDeProdutosActivity() {
             if (status > -1) {
                 Toast.makeText(this, "Produto Salvo", Toast.LENGTH_SHORT).show()
                 clearEditText()
-                getProducts()
             } else {
                 Toast.makeText(this, "Não foi possível salvar", Toast.LENGTH_SHORT).show()
             }
@@ -55,6 +45,13 @@ class SalvarProdutoActivity : ListaDeProdutosActivity() {
         editNome.setText("")
         editQuantidade.setText("")
         editNome.requestFocus()
+    }
+
+
+    private fun initView() {
+        editNome = findViewById(R.id.editTextNomeProduto)
+        editQuantidade = findViewById(R.id.editTextQuantidade)
+        buttonSalvar = findViewById(R.id.buttonSalvar)
     }
 }
 
